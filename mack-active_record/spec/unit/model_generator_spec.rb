77d003ed67062fccaadd0724c196d0636dcf6cba
Mark Bates <mark@markbates.com>
2008-06-16 17:28:19 -0400

    More stubbing of spec tests.

---
+++ mack-active_record/spec/unit/model_generator_spec.rb
@@ -0,0 +1,16 @@
+require 'pathname'
+require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
+
+describe ModelGenerator do
+  
+  it "should require a name for the model"
+  
+  it "should create a file for the model"
+  
+  it "should create a stub test/unit test for the model if test/unit is testing framework"
+  
+  it "should create a stub rspec test for the model if rspec is testing framework"
+  
+  it "should create a migration file"
+  
+end
---
+++ mack-active_record/spec/unit/scaffold_generator_spec.rb
@@ -0,0 +1,24 @@
+require 'pathname'
+require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
+
+describe ScaffoldGenerator do
+  
+  it "should require a name for the scaffold"
+  
+  it "should handle plural/singular names correctly"
+  
+  it "should update the routes.rb file with the resource name"
+  
+  it "should create a stub test/unit test for the controller if test/unit is testing framework"
+  
+  it "should create a stub rspec test for the controller if rspec is testing framework"
+  
+  it "should create a controller file"
+  
+  it "should create the proper view files"
+  
+  it "should create a model file"
+  
+  it "should create a migration file"
+  
+end
