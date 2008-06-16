06f4390018f20aea85b63a8dfa82324c8b684aa1
Mark Bates <mark@markbates.com>
2008-06-16 16:00:51 -0400

    Initial import.

---
+++ mack-active_record/test/mack-active_record_test.rb
@@ -0,0 +1,16 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+class MackActiveRecordTest < Test::Unit::TestCase
+
+  def setup
+  end
+  
+  def teardown
+  end
+  
+  # replace this with tests of your own.
+  def test_truth
+    assert true
+  end
+  
+end
---
+++ mack-active_record/test/test_helper.rb
@@ -0,0 +1,4 @@
+require "test/unit"
+
+# place common methods, assertions, and other type things in this file so
+# other tests will have access to them.
