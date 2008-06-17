02cebaafbadb3d1421ceea19007ba1db0f850a96
Mark Bates <mark@markbates.com>
2008-06-17 14:10:50 -0400

    Added hash_spec.rb

---
+++ mack-facets/spec/lib/extensions/hash_spec.rb
@@ -0,0 +1,26 @@
+require 'pathname'
+require Pathname(__FILE__).dirname.expand_path.parent.parent + 'spec_helper'
+
+describe Hash do
+  
+  describe "-" do
+    
+    it "should destructively removed elements in the Hash with the specified keys"
+    
+  end
+  
+  describe "join" do
+    
+    it "should return a string that is formatted with the proper join syntax that's specified"
+    
+  end
+  
+  describe "to_params" do
+    
+    it "should convert the Hash to a string representing query string parameters"
+    
+    it "should escape the values "
+    
+  end
+  
+end
