158aa304557ab75a2a879c81d93f5b5593f6472b
Mark Bates <mark@mackframework.com>
2008-06-18 11:27:50 -0400

    Added inflections_spec test.

---
+++ mack-facets/spec/lib/utils/inflections_spec.rb
@@ -0,0 +1,73 @@
+require 'pathname'
+require Pathname(__FILE__).dirname.expand_path.parent.parent + 'spec_helper'
+
+describe Mack::Utils::Inflector do
+  
+  def inflect
+    Mack::Utils::Inflector.instance
+  end
+  
+  describe "pluralize" do
+    
+    it "should return the plural of a given word" do
+      inflect.pluralize("error").should == "errors"
+      inflect.pluralize("errors").should == "errors"
+      inflect.pluralize("boats").should == "boats"
+      inflect.pluralize("sheep").should == "sheep"
+      inflect.pluralize("boat").should == "boats"
+      inflect.pluralize("girl").should == "girls"
+      inflect.pluralize("army").should == "armies"
+      inflect.pluralize("person").should == "people"
+      inflect.pluralize("equipment").should == "equipment"
+      inflect.pluralize("stimulus").should == "stimuli"
+      inflect.pluralize("quiz").should == "quizzes"
+    end
+    
+  end
+  
+  describe "singularize" do
+    
+    it "should return the singular of a given word" do
+      inflect.singularize("boat").should == "boat"
+      inflect.singularize("error").should == "error"
+      inflect.singularize("errors").should == "error"
+      inflect.singularize("sheep").should == "sheep"
+      inflect.singularize("boats").should == "boat"
+      inflect.singularize("girls").should == "girl"
+      inflect.singularize("armies").should == "army"
+      inflect.singularize("people").should == "person"
+      inflect.singularize("equipment").should == "equipment"
+      inflect.singularize("quizzes").should == "quiz"
+    end
+    
+  end
+  
+  describe "irregular" do
+    
+    it "should return the irregular version of a given word"
+    
+  end
+  
+  describe "rules" do
+    
+    describe "plural" do
+      
+      it "should except a pluralization rule and add it to the list"
+      
+    end
+    
+    describe "singular" do
+      
+      it "should except a singularization rule and add it to the list"
+      
+    end
+    
+    describe "irregular" do
+      
+      it "should except an irregular rule and add it to the list"
+      
+    end
+    
+  end
+  
+end
