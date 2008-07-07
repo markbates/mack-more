edd74ddb02123395d5a1f7592eb9ecf82597cb80
dsutedja <dsutedja@helium.com>
2008-07-07 09:30:42 -0400

    AR model generator

--- mack-active_record/spec/fake_application/db/migrations/004_create_zoos.rb
+++
@@ -1,9 +0,0 @@
-class CreateZoos < ActiveRecord::Migration
-
-  def self.up
-  end
-
-  def self.down
-  end
-
-end
--- mack-active_record/spec/lib/model_generator_spec.rb
+++ mack-active_record/spec/lib/model_generator_spec.rb
@@ -2,14 +2,15 @@ require 'pathname'
 require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
 
 describe ModelGenerator do
-  
-  before(:each) do
-    @file = File.join(Mack::Configuration.root, "app", "models", "zoo.rb")
-    FileUtils.rm_rf(@file)
+      
+  def cleanup(file)
+    File.delete(file) if File.exists?(file)
   end
   
   after(:each) do
-    FileUtils.rm_rf(@file)
+    cleanup(File.join(Mack::Configuration.root, "app", "models", "zoo.rb"))
+    cleanup(File.join(Mack::Configuration.root, "db", "migrations", "001_create_zoos.rb"))
+    cleanup(File.join(Mack::Configuration.root, "test", "unit", "zoo_test.rb"))
   end
   
   it "should require a name for the model" do
@@ -21,15 +22,27 @@ describe ModelGenerator do
   end
   
   it "should create a file for the model" do
-    File.exists?(@file).should_not == true
+    file = File.join(Mack::Configuration.root, "app", "models", "zoo.rb")
+    File.exists?(file).should_not == true
     ModelGenerator.run("NAME" => "zoo")
-    File.exists?(@file).should == true
-    puts File.read(@file)
+    File.exists?(file).should == true
+    
   end
   
-  it "should create a stub test/unit test for the model if test_case is testing framework"
+  it "should create a stub test/unit test for the model if test_case is testing framework" do
+    test_file = File.join(Mack::Configuration.root, "test", "unit", "zoo_test.rb")    
+    ModelGenerator.run("NAME" => "zoo")
+    File.exists?(test_file).should == true
+  end
   
-  it "should create a stub rspec test for the model if rspec is testing framework"
+  it "should create a stub rspec test for the model if rspec is testing framework" do
+    temp_app_config("mack::testing_framework" => "rspec") do
+      test_file = File.join(Mack::Configuration.root, "test", "spec", "lib", "zoo_spec.rb")
+      ModelGenerator.run("NAME" => "zoo")
+      File.exists?(test_file).should == true
+      File.delete(test_file)
+    end
+  end
   
   it "should create a migration file" do
     mig_file = File.join(migrations_directory, "001_create_zoos.rb")
