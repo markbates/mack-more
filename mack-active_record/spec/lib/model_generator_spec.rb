d6c450861baa963322bafd402e6c0659e3e10ba0
dsutedja <dsutedja@helium.com>
2008-07-07 15:36:18 -0400

    more AR generator spec cleanup

--- mack-active_record/spec/lib/model_generator_spec.rb
+++ mack-active_record/spec/lib/model_generator_spec.rb
@@ -3,10 +3,16 @@ require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
 
 describe ModelGenerator do
         
+  before(:each) do 
+    @model_file = File.join(Mack.root, "app", "models", "zoo.rb")
+    @mig_file   = File.join(Mack.root, "db", "migrations", "002_create_zoos.rb")
+    @test_file  = File.join(Mack.root, "test", "unit", "zoo_test.rb")
+  end
+  
   after(:each) do
-    cleanup(File.join(Mack.root, "app", "models", "zoo.rb"))
-    cleanup(File.join(Mack.root, "db", "migrations", "002_create_zoos.rb"))
-    cleanup(File.join(Mack.root, "test", "unit", "zoo_test.rb"))
+    cleanup(@model_file)
+    cleanup(@mig_file)
+    cleanup(@test_file)
   end
   
   it "should require a name for the model" do
@@ -18,34 +24,29 @@ describe ModelGenerator do
   end
   
   it "should create a file for the model" do
-    file = File.join(Mack.root, "app", "models", "zoo.rb")
-    File.exists?(file).should_not == true
+    File.exists?(@model_file).should_not == true
     ModelGenerator.run("NAME" => "zoo")
-    File.exists?(file).should == true
+    File.exists?(@model_file).should == true
     
   end
   
   it "should create a stub test/unit test for the model if test_case is testing framework" do
-    test_file = File.join(Mack.root, "test", "unit", "zoo_test.rb")    
     ModelGenerator.run("NAME" => "zoo")
-    File.exists?(test_file).should == true
+    File.exists?(@test_file).should == true
   end
   
   it "should create a stub rspec test for the model if rspec is testing framework" do
     temp_app_config("mack::testing_framework" => "rspec") do
-      test_file = File.join(Mack.root, "test", "unit", "zoo_spec.rb")
+      @test_file = File.join(Mack.root, "test", "unit", "zoo_spec.rb")
       ModelGenerator.run("NAME" => "zoo")
-      File.exists?(test_file).should == true
-      File.delete(test_file)
-      File.exists?(test_file).should_not == true
+      File.exists?(@test_file).should == true
     end
   end
   
   it "should create a migration file" do
-    mig_file = File.join(migrations_directory, "002_create_zoos.rb")
-    File.exists?(mig_file).should_not == true
+    File.exists?(@mig_file).should_not == true
     ModelGenerator.run("NAME" => "zoo", "cols" => "name:string,description:text,created_at:date_time,updated_at:date_time")
-    File.exists?(mig_file).should == true
+    File.exists?(@mig_file).should == true
   end
   
 end
