16e00a3209b0c96bd60e080bf4dc7d94eeec4f48
Mark Bates <mark@markbates.com>
2008-07-08 12:48:24 -0400

    Working on data_mapper transactional rspec tests.

--- mack-data_mapper/spec/lib/tasks/db_create_drop_tasks_spec.rb
+++ mack-data_mapper/spec/lib/tasks/db_create_drop_tasks_spec.rb
@@ -8,11 +8,19 @@ describe "rake" do
     describe "create" do
       
       def common_mysql_cleanup
-        repository(:test_tmp) do |repo|
+        repository(:mysql_test_tmp) do |repo|
           repo.adapter.execute "DROP DATABASE IF EXISTS `mack_data_mapper_development`"
           repo.adapter.execute "DROP DATABASE IF EXISTS `mack_data_mapper_test`"
           repo.adapter.execute "DROP DATABASE IF EXISTS `mack_data_mapper_production`"
         end
+      end
+      
+      def common_postgres_cleanup
+        # repository(:postgres_test_tmp) do |repo|
+        #   repo.adapter.execute "DROP DATABASE IF EXISTS mack_data_mapper_development"
+        #   repo.adapter.execute "DROP DATABASE IF EXISTS mack_data_mapper_test"
+        #   repo.adapter.execute "DROP DATABASE IF EXISTS mack_data_mapper_production"
+        # end
       end
       
       class Zombie
@@ -26,7 +34,7 @@ describe "rake" do
         before(:all) do
           @db_yml = File.read(Mack::Paths.config("database.yml"))
           File.open(Mack::Paths.config("database.yml"), "w") {|f| f.puts fixture("mysql_database.yml")}
-          DataMapper.setup(:test_tmp, "mysql://root@localhost/mysql")
+          DataMapper.setup(:mysql_test_tmp, "mysql://root@localhost/mysql")
           common_mysql_cleanup
         end
       
@@ -36,7 +44,7 @@ describe "rake" do
         end
       
         it "should create a db for the current environment" do
-          repository(:test_tmp) do |repo|
+          repository(:mysql_test_tmp) do |repo|
             repo.adapter.query("show databases;").should_not include("mack_data_mapper_test")
             Mack::Database.create
             repo.adapter.query("show databases;").should include("mack_data_mapper_test")
@@ -44,7 +52,7 @@ describe "rake" do
         end
       
         it "should drop/create a db if it already exists for the current environment" do
-          repository(:test_tmp) do |repo|
+          repository(:mysql_test_tmp) do |repo|
             Mack::Database.create
             Zombie.should_not be_storage_exist
             Zombie.auto_migrate!
@@ -55,7 +63,7 @@ describe "rake" do
         end
       
         it "should create a db for the specified environment" do
-          repository(:test_tmp) do |repo|
+          repository(:mysql_test_tmp) do |repo|
             repo.adapter.query("show databases;").should_not include("mack_data_mapper_production")
             Mack::Database.create("production")
             repo.adapter.query("show databases;").should include("mack_data_mapper_production")
@@ -63,7 +71,7 @@ describe "rake" do
         end
       
         it "should drop/create a db if it already exists for the specified environment" do
-          repository(:test_tmp) do |repo|
+          repository(:mysql_test_tmp) do |repo|
             Mack::Database.create("production")
             Zombie.should_not be_storage_exist
             Zombie.auto_migrate!
@@ -80,19 +88,60 @@ describe "rake" do
         before(:all) do
           @db_yml = File.read(Mack::Paths.config("database.yml"))
           File.open(Mack::Paths.config("database.yml"), "w") {|f| f.puts fixture("postgresql_database.yml")}
+          DataMapper.setup(:postgres_test_tmp, "postgres://ruby:password@localhost/postgres")
+          # common_postgres_cleanup
         end
         
         after(:all) do
           File.open(Mack::Paths.config("database.yml"), "w") {|f| f.puts @db_yml}
+          # common_postgres_cleanup
         end
         
-        it "should create a db for the current environment"
-        
-        it "should drop/create a db if it already exists for the current environment"
-        
-        it "should create a db for the specified environment"
-        
-        it "should drop/create a db if it already exists for the specified environment"
+        it "should create a db for the current environment" do
+          # rollback_transaction do
+            repository(:postgres_test_tmp) do |repo|
+              repo.adapter.query("select datname from pg_database").should_not include("mack_data_mapper_test")
+              Mack::Database.create
+              repo.adapter.query("select datname from pg_database").should include("mack_data_mapper_test")
+            end
+          # end
+        end
+      
+        it "should drop/create a db if it already exists for the current environment" do
+          # rollback_transaction do
+            repository(:postgres_test_tmp) do |repo|
+              Mack::Database.create
+              Zombie.should_not be_storage_exist
+              Zombie.auto_migrate!
+              Zombie.should be_storage_exist
+              Mack::Database.create
+              Zombie.should_not be_storage_exist
+            end
+          # end
+        end
+      
+        it "should create a db for the specified environment" do
+          # rollback_transaction do
+            repository(:postgres_test_tmp) do |repo|
+              repo.adapter.query("select datname from pg_database").should_not include("mack_data_mapper_production")
+              Mack::Database.create("production")
+              repo.adapter.query("select datname from pg_database").should include("mack_data_mapper_production")
+            end
+          # end
+        end
+      
+        it "should drop/create a db if it already exists for the specified environment" do
+          # rollback_transaction do
+            repository(:postgres_test_tmp) do |repo|
+              Mack::Database.create("production")
+              Zombie.should_not be_storage_exist
+              Zombie.auto_migrate!
+              Zombie.should be_storage_exist
+              Mack::Database.create("production")
+              Zombie.should_not be_storage_exist
+            end
+          # end
+        end
         
       end
     
@@ -103,15 +152,23 @@ describe "rake" do
           before(:all) do
             @db_yml = File.read(Mack::Paths.config("database.yml"))
             File.open(Mack::Paths.config("database.yml"), "w") {|f| f.puts fixture("mysql_database.yml")}
+            common_mysql_cleanup
           end
 
           after(:all) do
             File.open(Mack::Paths.config("database.yml"), "w") {|f| f.puts @db_yml}
+            common_mysql_cleanup
           end
           
-          it "should create a db for all environments"
-          
-          it "should drop/create a db all environments"
+          it "should create a db for all environments" do
+            repository(:mysql_test_tmp) do |repo|
+              repo.adapter.query("show databases;").should_not include("mack_data_mapper_test")
+              repo.adapter.query("show databases;").should_not include("mack_data_mapper_development")
+              rake_task("db:create:all")
+              repo.adapter.query("show databases;").should include("mack_data_mapper_test")
+              repo.adapter.query("show databases;").should include("mack_data_mapper_development")
+            end
+          end
           
         end
         
@@ -120,15 +177,15 @@ describe "rake" do
           before(:all) do
             @db_yml = File.read(Mack::Paths.config("database.yml"))
             File.open(Mack::Paths.config("database.yml"), "w") {|f| f.puts fixture("postgresql_database.yml")}
+            common_postgres_cleanup
           end
 
           after(:all) do
             File.open(Mack::Paths.config("database.yml"), "w") {|f| f.puts @db_yml}
+            common_postgres_cleanup
           end
           
           it "should create a db for all environments"
-          
-          it "should drop/create a db all environments"
           
         end
       
