376f67362cc1e4b1d90d0f1b289332bfeeed55cb
Mark Bates <mark@mackframework.com>
2008-07-23 11:09:59 -0400

    Added text.erb and html.erb templates for mailer.

--- mack-mailer/spec/lib/mailer_generator_spec.rb
+++ mack-mailer/spec/lib/mailer_generator_spec.rb
@@ -6,6 +6,8 @@ describe MailerGenerator do
     FileUtils.rm_rf(Mack::Paths.mailers)
     FileUtils.rm_rf(Mack::Paths.test)
     @mailer_file = Mack::Paths.mailers("registration_email.rb")
+    @text_file = Mack::Paths.mailers("registration_email", "text.erb")
+    @html_file = Mack::Paths.mailers("registration_email", "html.erb")
   end
   
   it "should require a name" do
@@ -19,7 +21,15 @@ describe MailerGenerator do
     File.read(@mailer_file).should == fixture("registration_email.rb")
   end
   
-  it "should create text.erb and html.erb files"
+  it "should create text.erb and html.erb files" do
+    File.should_not be_exists(@text_file)
+    File.should_not be_exists(@html_file)
+    MailerGenerator.run("name" => "registration_email")
+    File.should be_exists(@text_file)
+    File.read(@text_file).should == fixture("text.erb")
+    File.should be_exists(@html_file)
+    File.read(@html_file).should == fixture("html.erb")
+  end
   
   it "should create a spec test if using rspec"
   
