604a0c8fa5bf8664f2157e5c9c34b69b8028bec5
Mark Bates <mark@markbates.com>
2008-07-24 15:29:28 -0400

    Moved the testing extensions to a new file.

--- mack-mailer/lib/mack-mailer.rb
+++ mack-mailer/lib/mack-mailer.rb
@@ -13,50 +13,6 @@ end
 
 require File.join(File.dirname(__FILE__), "mailer")
 
+require File.join(File.dirname(__FILE__), "testing")
+
 require File.join(File.dirname(__FILE__), "loader")
-
-if Mack.env == "test"
-  
-  def delivered_emails
-    Mack::Mailer::DeliveryHandlers::Test::EmailRegistry.registered_items
-  end
-  
-  module Spec # :nodoc:
-    module Example # :nodoc:
-      module ExampleMethods # :nodoc:
-        include Mack::Routes::Urls
-        include Mack::Testing::Helpers
-
-        alias_method :email_spec_execute, :execute
-
-        def execute(options, instance_variables)
-          @__res = email_spec_execute(options, instance_variables)
-          Mack::Mailer::DeliveryHandlers::Test::EmailRegistry.reset!
-          @__res
-        end
-
-      end # ExampleMethods
-    end # Example
-  end # Spec
-  
-  module Test # :nodoc:
-    module Unit # :nodoc:
-      class TestCase # :nodoc:
-
-        # Let's alias the run method in the class above us so we can create a new one here
-        # but still reference it.
-        alias_method :super_run, :run # :nodoc:
-
-        # We need to wrap the run method so we can do things like
-        # run a cleanup method if it exists
-        def run(result, &progress_block) # :nodoc:
-          @__res = super_run(result)
-          Mack::Mailer::DeliveryHandlers::Test::EmailRegistry.reset!
-          @__res
-        end
-
-      end # TestCase
-    end # Unit
-  end # Test
-  
-end
