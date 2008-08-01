4f81e36425506b164dd8af6a34ce559e3eebe08b
Mark Bates <mark@markbates.com>
2008-08-01 13:54:23 -0400

    Patched data_mapper to allow for the setting of created_at and created_on on explicitly.

---
+++ mack-data_mapper/lib/mack-data_mapper/dm_patches/dm-timestamps.rb
@@ -0,0 +1,6 @@
+module DataMapper
+  module Timestamp
+    TIMESTAMP_PROPERTIES[:created_at] = lambda { |r| r.created_at = DateTime.now if r.new_record? && r.created_at.nil? }
+    TIMESTAMP_PROPERTIES[:created_on] = lambda { |r| r.created_on = DateTime.now if r.new_record? && r.created_on.nil? }
+  end
+end
