aba6db213b65492efdfa8c081e53219b1dc4f31f
Mark Bates <mark@markbates.com>
2008-08-04 14:05:09 -0400

    Fixed: Serial data type in DataMapper doesn't work with SQLite3 [#72 state:resolved]

--- mack-data_mapper/lib/mack-data_mapper/dm_patches/migrations.rb
+++ mack-data_mapper/lib/mack-data_mapper/dm_patches/migrations.rb
@@ -7,3 +7,15 @@ module DataMapper # :nodoc:
     
   end
 end
+
+module SQL # :nodoc:
+  module Sqlite3 # :nodoc:
+
+    def property_schema_statement(schema)
+      statement = super
+      statement << ' PRIMARY KEY AUTOINCREMENT' if supports_serial? && schema[:serial]
+      statement
+    end
+ 
+  end
+end
