5758548c727a8ae815609f7489dc6f00c06d139e
dsutedja <dsutedja@helium.com>
2008-08-05 14:26:30 -0400

    Moved ring_server tasks to mack-distributed from mack core [#75 state:resolved]

--- mack-distributed/lib/mack-distributed.rb
+++ mack-distributed/lib/mack-distributed.rb
@@ -1,15 +1,11 @@
 base = File.join(File.dirname(__FILE__), "mack-distributed")
 
-def traverse_and_require(path)
-  Dir.entries(path).each do |file|
-    next if file == "." or file == ".."
-    full_path = File.join(path, file)
-    if FileTest.directory?(full_path)
-      traverse_and_require(full_path)
-    else
-      require full_path
-    end
-  end
+# load *.rb files
+Dir.glob(File.join(base, "**", "*.rb")).each do |f|
+  load(f)
 end
 
-traverse_and_require(base)
+# load tasks
+Dir.glob(File.join(base, "tasks", "*.rake")).each do |f|
+  load(f)
+end
