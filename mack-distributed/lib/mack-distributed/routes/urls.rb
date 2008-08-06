fa64e5f6e217a9b8a4a33cfb09a5a41f846c32b4
Mark Bates <mark@mackframework.com>
2008-08-06 14:56:11 -0400

    Just a few more tests to go with renaming use_distributed_routes to share_routes. [#79]

--- mack-distributed/lib/mack-distributed/routes/urls.rb
+++ mack-distributed/lib/mack-distributed/routes/urls.rb
@@ -32,6 +32,51 @@ module Mack
       
     end # Routes
   end # Distributed
+  
+  module Routes # :nodoc:
+    module Urls
+      # Retrieves a distributed route from a DRb server.
+      # 
+      # Example:
+      #   droute_url(:app_1, :home_page_url)
+      #   droute_url(:registration_app, :signup_url, {:from => :google})
+      def droute_url(app_name, route_name, options = {})
+        if app_config.mack.share_routes
+          d_urls = Mack::Distributed::Routes::Urls.get(app_name)
+          # return d_urls.send(route_name, options)
+          # ivar_cache("droute_url_hash") do
+          #   {}
+          # end
+          # d_urls = @droute_url_hash[app_name.to_sym]
+          # if d_urls.nil?
+          #   d_urls = Mack::Distributed::Routes::UrlCache.get(app_name.to_sym)
+          #   @droute_url_hash[app_name.to_sym] = d_urls
+          #   if d_urls.nil?
+          #     raise Mack::Distributed::Errors::UnknownApplication.new(app_name)
+          #   end
+          # end
+          route_name = route_name.to_s
+          if route_name.match(/_url$/)
+            unless route_name.match(/_distributed_url$/)
+              route_name.gsub!("_url", "_distributed_url")
+            end
+          else
+            route_name << "_distributed_url"
+          end
+          raise Mack::Distributed::Errors::UnknownRouteName.new(app_name, route_name) unless d_urls.respond_to?(route_name)
+          return d_urls.run(route_name, options)
+          # if d_urls.run.respond_to?(route_name)
+          #   return d_urls.run.send(route_name, options)
+          # else
+          #   raise Mack::Distributed::Errors::UnknownRouteName.new(app_name, route_name)
+          # end
+        else
+          return nil
+        end
+      end # droute_url
+    end # Urls
+  end # Routes
+  
 end # Mack
 
 Mack::Routes.after_class_method(:build) do
