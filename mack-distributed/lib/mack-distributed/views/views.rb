fd79b4c794c14401ffb3ff08add20c24117484d3
dsutedja <dsutedja@helium.com>
2008-08-12 11:33:02 -0400

    test for dist_views

--- mack-distributed/lib/mack-distributed/views/views.rb
+++ mack-distributed/lib/mack-distributed/views/views.rb
@@ -6,8 +6,9 @@ module Mack
       include DRbUndumped
       
       def get(resource)
-        if File.exists?(resource)
-          return File.read(resource)
+        path = File.join(Mack.root, resource)
+        if File.exists?(path)
+          return File.read(path)
         end
         return ""
       end
@@ -43,24 +44,6 @@ module Mack
           
           return nil
         end
-        
-        # def register_files
-        #   if app_config.mack.share_layouts
-        #     raise Mack::Distributed::Errors::ApplicationNameUndefined.new if app_config.mack.distributed_app_name.nil?
-        #     
-        #     base = File.join(Mack.root, "app", "views", "**", "*.*")
-        #     Mack.logger.info "Registering Distributed View Files for #{app_config.mack.distributed_app_name}:"
-        #     Dir.glob(base) do |file|
-        #       file_name = File.basename(file)              
-        #       resource = file[Mack.root.size+1, file.size-1]
-        #       
-        #       Mack.logger.info "  * :space => #{app_config.mack.distributed_app_name.to_sym}, :klass_def => #{resource}, :object => #{File.read(file)}"
-        #       Mack::Distributed::Utils::Rinda.register_or_renew(:space => app_config.mack.distributed_app_name.to_sym, 
-        #                                                         :klass_def => resource, 
-        #                                                         :object => File.read(file), :timeout => 0)
-        #     end
-        #   end
-        # end
       end
     end
   end
--- mack-distributed/spec/lib/distributed/views/layout_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.join(File.dirname(__FILE__), "..", "..", "..", "spec_helper")
-
-describe "Distributed Layout" do
-  
-  it "should alias Mack::Rendering::Type::Layout.render as local_render" do
-    Mack::Rendering::Type::Layout.instance_methods.include?("local_render").should == true
-  end
-  
-end
