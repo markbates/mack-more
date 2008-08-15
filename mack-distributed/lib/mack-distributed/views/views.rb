f6f5f93fef03bbb8f049d18327563774597c1ed2
dsutedja <dsutedja@helium.com>
2008-08-15 17:20:47 -0400

    Distributed views module now uses file cache [#96 state:resolved]

--- mack-distributed/lib/mack-distributed/views/views.rb
+++ mack-distributed/lib/mack-distributed/views/views.rb
@@ -1,27 +1,42 @@
 module Mack
   module Distributed
+    
+    class FileCache < Cachetastic::Caches::Base
+      class << self
+        include Mack::ViewHelpers::LinkHelpers
+      
+        def get(path)
+          raw = super(path) do
+            raw = ""
+            if File.exists?(path)
+              raw = File.read(path)
+
+              # preprocess the raw content so we can resolve css/javascript/image path
+              arr = raw.scan(/<%=.*?%>/)
+              arr.each do |scriptlet|
+                if scriptlet.match(/stylesheet/) or scriptlet.match(/javascript/) or scriptlet.match(/image/)
+                  res = ERB.new(scriptlet).result(binding)
+                  raw.gsub!(scriptlet, res)
+                end 
+              end # if arr.each
+            end # if File.exists?
+            
+            set(path, raw)
+          end # super(key)
+          return raw
+        end # def get
+      end # class << self
+    end # class FileCache
+    
     class Views
       
       include Singleton
       include DRbUndumped
-      include Mack::ViewHelpers::LinkHelpers
       
       def get(resource)
         path = File.join(Mack.root, resource)
-        if File.exists?(path)
-          raw = File.read(path)
-          
-          # preprocess the raw content so we can resolve css/javascript/image path
-          arr = raw.scan(/<%=.*?%>/)
-          arr.each do |scriptlet|
-            if scriptlet.match(/stylesheet/) or scriptlet.match(/javascript/) or scriptlet.match(/image/)
-              res = ERB.new(scriptlet).result(binding)
-              raw.gsub!(scriptlet, res)
-            end
-          end
-          return raw
-        end
-        return ""
+        raw = FileCache.get(path)
+        return raw
       end
       
       class << self
