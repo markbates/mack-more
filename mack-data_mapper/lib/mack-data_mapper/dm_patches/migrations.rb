afe7123c2dcdc697474aaf48de8ae23950eef71e
Mark Bates <mark@mackframework.com>
2008-08-20 18:00:51 -0400

    Added patch to DM for change_column and add_column

--- mack-data_mapper/lib/mack-data_mapper/dm_patches/migrations.rb
+++ mack-data_mapper/lib/mack-data_mapper/dm_patches/migrations.rb
@@ -17,5 +17,27 @@ module SQL # :nodoc:
       statement
     end
  
-  end
-end
+  end # Sqlite3
+  
+  class TableModifier # :nodoc:
+
+    def add_column(name, type, opts = {})
+      column = SQL::TableCreator::Column.new(@adapter, name, build_type(name, type), opts)
+      @statements << "ALTER TABLE #{quoted_table_name} ADD COLUMN #{column.to_sql}"
+    end
+
+    def change_column(name, type, opts = {})
+      # raise NotImplemented for SQLite3
+      @statements << "ALTER TABLE #{quoted_table_name} ALTER COLUMN #{quote_column_name(name)} TYPE #{build_type(name, type, opts).gsub(quote_column_name(name), '').gsub('NOT NULL', '')}"
+    end
+
+    def build_type(name, type_class, options = @opts)
+      schema = {:name => name.to_s, :quote_column_name => quote_column_name(name)}.merge(options)
+      schema[:serial?] ||= schema[:serial]
+      schema[:nullable?] ||= schema[:nullable]
+      schema = @adapter.class.type_map[type_class].merge(schema)
+      @adapter.property_schema_statement(schema)
+    end
+
+  end # TableModifier  
+end # SQL
