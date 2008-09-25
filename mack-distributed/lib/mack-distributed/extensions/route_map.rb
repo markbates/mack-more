1b5d06476b8704396b8f7288d4aa6fd71f0b19ba
Mark Bates <mark@markbates.com>
2008-09-25 11:58:56 -0400

    Updated mack-distributed to use the new routes.

--- mack-distributed/lib/mack-distributed/extensions/route_map.rb
+++ mack-distributed/lib/mack-distributed/extensions/route_map.rb
@@ -5,11 +5,11 @@ module Mack
       # let's make sure we only do the alias_method once.
       unless self.private_instance_methods.include?("normal_connect_with_named_route")
         
-        alias_method :normal_connect_with_named_route, :connect_with_named_route
+        alias_method :normal_connect_with_name, :connect_with_name
       
-        def connect_with_named_route(n_route, pattern, options = {}) # :nodoc:
-          n_route = n_route.methodize
-          normal_connect_with_named_route(n_route, pattern, options)
+        def connect_with_name(name, path, options = {}, &block) # :nodoc:
+          n_route = name.methodize
+          normal_connect_with_name(n_route, path, options, &block)
           if configatron.mack.distributed.share_routes
             Mack::Routes::Urls.class_eval %{
               def #{n_route}_distributed_url(options = {})
