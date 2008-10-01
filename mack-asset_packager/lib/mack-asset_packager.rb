2b246e05d3b914b7a2f46314d3d1ad930a255603
Mark Bates <mark@mackframework.com>
2008-10-01 10:39:06 -0400

    Commented out puts statement.

--- mack-asset_packager/lib/mack-asset_packager.rb
+++ mack-asset_packager/lib/mack-asset_packager.rb
@@ -2,7 +2,7 @@ fl = File.join(File.dirname(__FILE__), "mack-asset_packager")
 
 [:link_helpers, :package].each do |f|
   file = File.join(fl, "#{f}.rb")
-  puts "requiring #{file}"
+  # puts "requiring #{file}"
   require file
 end
 
