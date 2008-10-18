7d6dc22de95775060e6e7e6e4847d92455577c5a
dsutedja <dsutedja@helium.com>
2008-10-18 08:38:09 -0400

    Jabber support (including ability to send to multiple recipients) [#78 state:resolved] Configatron update for mack-notifier [#200 state:resolved]

--- mack-notifier/lib/mack-notifier/errors.rb
+++ mack-notifier/lib/mack-notifier/errors.rb
@@ -10,7 +10,23 @@ module Mack
     class XmppError < StandardError
       def initialize(msg)
         super(msg)
+        self.error_hash = {}
       end
+      
+      def add_error(type, msg)
+        self.error_hash[type] ||= []
+        self.error_hash[type] << msg
+      end
+      
+      def get_error(type)
+        self.error_hash[type]
+      end
+      
+      def empty?
+        self.error_hash.empty?
+      end
+      
+      attr_accessor :error_hash
     end
     
     class XmppAuthenticationError < StandardError
