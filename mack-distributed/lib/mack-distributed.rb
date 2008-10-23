4812b4ac492af87a1f09f18397793784bcd2d97a
dsutedja <dsutedja@helium.com>
2008-10-23 12:00:25 -0400

    removing default value for mack.distributed.site_domain [#215 state:resolved]

--- mack-distributed/lib/mack-distributed.rb
+++ mack-distributed/lib/mack-distributed.rb
@@ -9,7 +9,7 @@ configatron.mack.distributed.set_default(:share_routes, false)
 configatron.mack.distributed.set_default(:share_objects, false)
 configatron.mack.distributed.set_default(:share_views, false)
 configatron.mack.distributed.set_default(:app_name, nil)
-configatron.mack.distributed.set_default(:site_domain, 'http://localhost:3000')
+configatron.mack.distributed.set_default(:site_domain, nil)
 configatron.mack.distributed.set_default(:timeout, 0)
 
 # load *.rb files
