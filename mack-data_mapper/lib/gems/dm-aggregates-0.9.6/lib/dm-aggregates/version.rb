8f8daa11e4c0607e087f8d8769ef337e03412402
Mark Bates <mark@mackframework.com>
2008-11-27 11:53:44 -0500

    Updated to DM 0.9.7

--- mack-data_mapper/lib/gems/dm-aggregates-0.9.6/lib/dm-aggregates/version.rb
+++
@@ -1,7 +0,0 @@
-module DataMapper
-  module More
-    module Aggregates
-      VERSION = "0.9.6"
-    end
-  end
-end
--- mack-data_mapper/lib/gems/dm-timestamps-0.9.6/lib/dm-timestamps/version.rb
+++
@@ -1,5 +0,0 @@
-module DataMapper
-  module Timestamps
-    VERSION = "0.9.6"
-  end
-end
