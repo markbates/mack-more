c0f0cdc8c162a99c7fedf7c1cee5981806c82608
Mark Bates <mark@mackframework.com>
2009-01-01 18:27:28 -0500

    Added test/unit/error.rb for active_support 1.9 support.

--- mack-facets/lib/mack-facets/1_9/test/unit/error.rb
+++ mack-facets/lib/mack-facets/1_9/test/unit/error.rb
@@ -1,10 +1,13 @@
-module Test
-  module Unit
+# This class is only here because active_support 2.2.2 requires test/unit/error,
+# and apparently that does not exist in Ruby 1.9. This, should hopefully, go away
+# in a future version of active_support.
+module Test # :nodoc:
+  module Unit # :nodoc:
 
     class Error # :nodoc:
       
       def message(*args)
-        
+        Mack.logger.error(args.inspect)
       end
       
     end
